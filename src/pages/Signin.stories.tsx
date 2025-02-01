7559d0cbe32c0da0b48935a11043d777d77e4da3
Lucas Thomaz <115643290+lucas-thomaz-design@users.noreply.github.com>
2025-02-01 22:25:23 +0100

    Update Signin.stories.tsx

--- src/pages/Signin.stories.tsx
+++ src/pages/Signin.stories.tsx
@@ -16,7 +16,7 @@ export default {
             handlers:[
                 rest.post('/sessions', (req, res, ctx) => {
                     return res(ctx.json({
-                        message: "Login realizado!"
+                        message: 'Login realizado!'
                         
                     }))
                 } )
